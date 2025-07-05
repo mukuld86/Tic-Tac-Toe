34b0d79f3f93d77d66540c1d0680f31bcae50c8a
mukuld86 <mukul.deshwal@outlook.com>
2025-07-05 17:10:56 +0530

    Added functionality to reset and new game buttons

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#newGame-btn");
+let msgContainer = document.querySelector(".hide");
+let msg = document.querySelector("#msg");
+let scores = document.querySelector("#scores");
 
+let scoreO = 0;
+let scoreX = 0;
 let turnO = true;
+let gameOver = false;
 
 const winPatterns = [
     [0, 1, 2],
@@ -14,26 +21,53 @@ const winPatterns = [
     [2, 4, 6]
 ];
 
-function resetGame() {
-    console.log("Resetting game");
+newGameBtn.addEventListener("click", () => {
+    console.log("New game started");
     boxes.forEach((box) => {
         box.innerText = "";
         box.disabled = false;
     });
+    msg.innerText = "";
     turnO = true;
+    gameOver=false;
+});
+
+resetBtn.addEventListener("click", ()=>{
+    console.log("Resetting game");
+    boxes.forEach((box) => {
+        box.innerText = "";   
+        box.disabled = false;
+    });
+    turnO = true;
+    gameOver = false;
+    msgContainer.classList.add("hide");
+    msgContainer.classList.remove("msg-container");
+});
+
+const showWinner = (winner) => {
+    msgContainer.classList.remove("hide");
+    msgContainer.classList.add("msg-container");
+    msg.innerText = "User " + winner + " won the game!";
+    msg.style.fontSize = "5vmin";
+    if(turnO){
+        scoreX++;
+    }else{
+        scoreO++;
+    }
+    scores.innerText = `Player O: ${scoreO} points
+    Player X: ${scoreX} points`;
 }
 
 function won() {
+    gameOver = true;
     console.log("Game Over");
     boxes.forEach((box) => {
         box.disabled = true;
     });
-    alert("User " + (turnO ? "X" : "O") + " won the game!");
+    showWinner(turnO ? "X" : "O");
 }
 const checkWinner = () => {
     for(let pattern of winPatterns) {
-        // console.log(pattern[0],pattern[1],pattern[2]);
-        // console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
@@ -43,9 +77,11 @@ const checkWinner = () => {
         };
     }
 } 
+
+
+
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        console.log("Box clicked");
         if(turnO){
             turnO = false;
             box.innerText = "O";
@@ -58,4 +94,3 @@ boxes.forEach((box) => {
         checkWinner();
     })
 })
-resetBtn.addEventListener("click", resetGame);
